0d66a4074f708e7c4774f357a9528b23be8650aa
KITEG21 <150097269+KITEG21@users.noreply.github.com>
2025-02-24 18:52:01 -0500

    Eleventh Commit

--- FullWebApi.Api/Migrations/20250224234118_RenderInit.Designer.cs
+++ FullWebApi.Api/Migrations/20250224234118_RenderInit.Designer.cs
@@ -2,6 +2,7 @@
 using FullWebApi.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FullWebApi.Api.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250224234118_RenderInit")]
+    partial class RenderInit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
